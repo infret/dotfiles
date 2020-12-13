74a0697b3e84671acd4bb3a9c98e29c28047ebb8
infret <infr3t@gmail.com>
2020-12-13 12:28:17 +0300

    Move files to XDG

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,10 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 set fish_greeting
 
+set PM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
+set _JAVA_OPTIONS -Djava.util.prefs.userRoot='$XDG_CONFIG_HOME'/java
+set XAUTHORITY "$XDG_RUNTIME_DIR"/Xauthority
+
 function fish_prompt
     printf '%s%s%s%s > ' (set_color '#790079') (prompt_pwd)
 end
