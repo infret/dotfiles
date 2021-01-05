20a9acd8daaace6802936b08815b5bfa62a5f25e
infret <infr3t@gmail.com>
2021-01-05 12:21:55 +0000

    Add shortcuts and fix battery script

--- .config/openbox/battery.sh
+++ .config/openbox/battery.sh
@@ -1,9 +1,14 @@
-while :
-  do
-    sleep 1
-    if [[ $(cat /sys/class/power_supply/BAT0/capacity) -lt "20" ]]; then
-      echo 0 > /sys/class/leds/tpacpi\:\:power/brightness
-      sleep 1
-      echo 255 > /sys/class/leds/tpacpi\:\:power/brightness
-    fi
+while true; do
+  if [[ $(cat /sys/class/power_supply/BAT0/capacity) < "10" ]]; then
+    while true; do
+      echo 0 >/sys/class/leds/tpacpi::power/brightness
+      sleep 1s
+      echo 255 >/sys/class/leds/tpacpi::power/brightness
+      sleep 1s
+      if [[ $(cat /sys/class/power_supply/BAT0/capacity) > "10" ]]; then
+        break
+      fi
+    done
+  fi
+  sleep 1m
 done
