dea9036acfb7ce28c26be1ec7321d485965ba673
infret <infr3t@gmail.com>
2021-01-26 20:54:39 +0000

    Add firefox

---
+++ .mozilla/firefox/hp2n5hx9.default-release/user.js
@@ -0,0 +1,124 @@
+user_pref('accessibility.typeaheadfind.flashBar', 0)
+user_pref('app.normandy.first_run', false)
+user_pref('app.normandy.migrationsApplied', 10)
+user_pref(
+  'app.normandy.startupExperimentPrefs.extensions.pocket.loggedOutVariant',
+  'variant_c'
+)
+user_pref('browser.bookmarks.restore_default_bookmarks', false)
+user_pref('browser.bookmarks.showMobileBookmarks', true)
+user_pref('browser.contentblocking.category', 'strict')
+user_pref('browser.ctrlTab.recentlyUsedOrder', false)
+user_pref('browser.download.autohideButton', false)
+user_pref('browser.download.dir', '/home/user')
+user_pref('browser.download.folderList', 2)
+user_pref('browser.download.panel.shown', true)
+user_pref('browser.download.viewableInternally.typeWasRegistered.svg', true)
+user_pref('browser.download.viewableInternally.typeWasRegistered.webp', true)
+user_pref('browser.download.viewableInternally.typeWasRegistered.xml', true)
+user_pref('browser.laterrun.enabled', true)
+user_pref(
+  'browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons',
+  false
+)
+user_pref(
+  'browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features',
+  false
+)
+user_pref('browser.newtabpage.enabled', false)
+user_pref('browser.newtabpage.storageVersion', 1)
+user_pref(
+  'browser.pageActions.persistedActions',
+  '{"version":1,"ids":["bookmark","pinTab","bookmarkSeparator","copyURL","emailLink","addSearchEngine","sendToDevice","pocket","screenshots_mozilla_org"],"idsInUrlbar":[]}'
+)
+user_pref('browser.pagethumbnails.storage_version', 3)
+user_pref('browser.preferences.defaultPerformanceSettings.enabled', true)
+user_pref('browser.protections_panel.infoMessage.seen', true)
+user_pref('browser.rights.3.shown', true)
+user_pref('browser.search.region', 'RU')
+user_pref('browser.shell.checkDefaultBrowser', true)
+user_pref('browser.startup.homepage', 'about:blank')
+user_pref(
+  'browser.uiCustomization.state',
+  '{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","urlbar-container","downloads-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button","ublock0_raymondhill_net-browser-action","_290ce447-2abb-4d96-8384-7256dd4a1c43_-browser-action"],"dirtyAreaCache":["nav-bar","toolbar-menubar","TabsToolbar","PersonalToolbar"],"currentVersion":16,"newElementCount":3}'
+)
+user_pref('browser.urlbar.placeholderName', 'Google')
+user_pref('browser.urlbar.placeholderName.private', 'Google')
+user_pref('browser.urlbar.tabToSearch.onboard.interactionsLeft', 2)
+user_pref('devtools.cache.disabled', true)
+user_pref('devtools.debugger.prefs-schema-version', 11)
+user_pref(
+  'devtools.netmonitor.columnsData',
+  '[{"name":"status","minWidth":30,"width":6.67},{"name":"method","minWidth":30,"width":6.67},{"name":"domain","minWidth":30,"width":13.33},{"name":"file","minWidth":30,"width":33.33},{"name":"url","minWidth":30,"width":25},{"name":"initiator","minWidth":30,"width":13.33},{"name":"type","minWidth":30,"width":6.67},{"name":"transferred","minWidth":30,"width":13.33},{"name":"contentSize","minWidth":30,"width":6.67},{"name":"waterfall","minWidth":150,"width":15}]'
+)
+user_pref('devtools.netmonitor.msg.visibleColumns', '["data","time"]')
+user_pref('devtools.selfxss.count', 5)
+user_pref('devtools.toolbox.host', 'right')
+user_pref('devtools.toolbox.previousHost', 'bottom')
+user_pref('devtools.toolbox.selectedTool', 'webconsole')
+user_pref('devtools.toolbox.sidebar.width', 381)
+user_pref('devtools.toolbox.splitconsoleEnabled', true)
+user_pref('devtools.toolsidebar-height.inspector', 350)
+user_pref('devtools.toolsidebar-width.inspector', 913)
+user_pref('devtools.toolsidebar-width.inspector.splitsidebar', 250)
+user_pref('devtools.webconsole.input.editorOnboarding', false)
+user_pref('devtools.webconsole.input.editorWidth', 309)
+user_pref('distribution.archlinux.bookmarksProcessed', true)
+user_pref('distribution.iniFile.exists.value', true)
+user_pref('doh-rollout.balrog-migration-done', true)
+user_pref('doh-rollout.doneFirstRun', true)
+user_pref('extensions.activeThemeID', 'firefox-compact-light@mozilla.org')
+user_pref('extensions.blocklist.pingCountVersion', -1)
+user_pref('extensions.databaseSchema', 33)
+user_pref('extensions.fxmonitor.firstAlertShown', true)
+user_pref('extensions.getAddons.databaseSchema', 6)
+user_pref('extensions.incognito.migrated', true)
+user_pref('extensions.pendingOperations', false)
+user_pref('extensions.systemAddonSet', '{"schema":1,"addons":{}}')
+user_pref('extensions.ui.dictionary.hidden', true)
+user_pref('extensions.ui.extension.hidden', false)
+user_pref('extensions.ui.lastCategory', 'addons://list/extension')
+user_pref('extensions.ui.locale.hidden', true)
+user_pref('extensions.webcompat.enable_picture_in_picture_overrides', true)
+user_pref('extensions.webcompat.enable_shims', true)
+user_pref('extensions.webcompat.perform_injections', true)
+user_pref('extensions.webcompat.perform_ua_overrides', true)
+user_pref(
+  'extensions.webextensions.ExtensionStorageIDB.migrated.screenshots@mozilla.org',
+  true
+)
+user_pref(
+  'extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net',
+  true
+)
+user_pref('font.internaluseonly.changed', false)
+user_pref('font.language.group', 'x-cyrillic')
+user_pref('font.name.sans-serif.x-cyrillic', 'Liberation Sans')
+user_pref('font.name.sans-serif.x-western', 'Liberation Sans')
+user_pref('font.name.serif.x-cyrillic', 'Liberation Serif')
+user_pref('font.name.serif.x-western', 'Liberation Serif')
+user_pref('layout.spellcheckDefault', 0)
+user_pref('media.autoplay.default', 5)
+user_pref('network.dns.disablePrefetch', true)
+user_pref('network.http.speculative-parallel-limit', 0)
+user_pref('network.predictor.enabled', false)
+user_pref('network.prefetch-next', false)
+user_pref('network.trr.blocklist_cleanup_done', true)
+user_pref('pdfjs.enabledCache.state', true)
+user_pref('pdfjs.migrationVersion', 2)
+user_pref('permissions.default.desktop-notification', 2)
+user_pref('places.database.lastMaintenance', 1610562392)
+user_pref('places.history.expiration.transient_current_max_pages', 101476)
+user_pref('pref.general.disable_button.default_browser', false)
+user_pref('privacy.purge_trackers.date_in_cookie_database', '0')
+user_pref('privacy.purge_trackers.last_purge', '1610562392249')
+user_pref(
+  'privacy.sanitize.pending',
+  '[{"id":"newtab-container","itemsToClear":[],"options":{}}]'
+)
+user_pref('privacy.trackingprotection.enabled', true)
+user_pref('privacy.trackingprotection.socialtracking.enabled', true)
+user_pref('toolkit.telemetry.pioneer-new-studies-available', false)
+user_pref('toolkit.telemetry.reportingpolicy.firstRun', false)
+user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true)
+user_pref('trailhead.firstrun.didSeeAboutWelcome', true)
