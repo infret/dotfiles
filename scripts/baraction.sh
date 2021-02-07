ff5ec1f76c845c042cab8ba11c235e9197e78df0
infret <infr3t@gmail.com>
2021-02-07 09:20:27 +0000

    Update spectrwm configuration

--- scripts/baraction.sh
+++ scripts/baraction.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-SLEEP_SEC=5 
-COUNT=0
 while :; do
-	let COUNT=$COUNT+1
-        echo -e "$(date +'%a, %b %d %R') $(cat /sys/class/power_supply/BAT0/capacity)% $(vmstat -s -S m | grep 'free mem' | awk '{print $1;}')M"
-        sleep $SLEEP_SEC
+  echo -e "$(date +'%a, %b %d %R') $(cat /sys/class/power_supply/BAT0/capacity)% $(vmstat -s -S m | grep 'free mem' | awk '{print $1;}')M $(expr 100 + $(awk -F'[][]' '/dB/ { print $4 }' <(amixer sget Master) | cut -c -3))dB"
+        sleep 5
 done
