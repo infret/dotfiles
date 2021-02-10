39cdbcd62eb6742597b265b7470507a233276311
infret <infr3t@gmail.com>
2021-02-10 19:52:33 +0000

    Update fonts, rewrite spectrwm panel

--- .bashrc
+++ .bashrc
@@ -2,7 +2,7 @@ alias ..='cd ..'
 alias ls='ls -aCF --color=auto'
 alias rm='rm -rf'
 alias mkdir='mkdir -pv'
-alias free='free -mt'
+alias free='free --mega'
 
 export LC_ALL=en_US.UTF-8
 PS1="\[\e[35m\]\w\[\e[m\]\[\e[35m\] >\[\e[m\] "
--- .config/baraction.sh
+++ .config/baraction.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
+bat() {
+batstat="$(cat /sys/class/power_supply/BAT0/status)"
+battery="$(cat /sys/class/power_supply/BAT0/capacity)"
+if [ $batstat = 'Unknown' ]; then
+  batstat=""
+  elif [[ $battery -le 19 ]]; then
+    batstat="+@fg=1; "
+  elif [[ $battery -ge 20 ]] && [[ $battery -le 39 ]]; then
+    batstat=" "
+  elif [[ $battery -ge 40 ]] && [[ $battery -le 69 ]]; then
+    batstat=" "
+  elif [[ $battery -ge 70 ]] && [[ $battery -le 89 ]]; then
+    batstat=" "
+  elif [[ $battery -ge 90 ]]; then
+    batstat=" "
+fi
+
+echo "$batstat ${battery}%"
+}
+
+mem(){
+  mem="$(free --mega | grep Mem | awk '{print $3}')"
+  if [ "$mem" -gt "3500" ];
+  then
+    echo -e "+@fg=1;  ${mem}M"
+  else 
+    echo -e "+@fg=0;  ${mem}M"
+  fi
+}
+vol(){
+  vol="$(pactl list sinks | grep '^[[:space:]]Volume:' | awk '{print $5}')"
+  stat="$(pacmd list-sinks | grep 'muted: yes')"
+  if [ -z $stat ];
+  then
+    if [ "${vol%?}" -gt "32" ];
+    then 
+      echo -e "+@fg=1;  ${vol}"
+    else
+      echo -e "+@fg=0; 墳 ${vol}"
+    fi
+  else
+    echo -e "+@fg=1; 婢 0%"
+  fi
+}
 while :; do
-  echo -e "$(date +'%a, %b %d %R') $(cat /sys/class/power_supply/BAT0/capacity)% $(vmstat -s -S m | grep 'free mem' | awk '{print $1;}')M $(expr 100 + $(awk -F'[][]' '/dB/ { print $4 }' <(amixer sget Master) | cut -c -3))dB"
-        sleep 2
+  echo -e "$(bat) $(mem) $(vol)"
+  sleep 1
 done
